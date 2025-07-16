1e87c402a9154bc9bbf95c8650ae49a4e6893ff7
Jhon Jurado <jhonedisonjurado59@gmail.com>
2025-07-16 12:44:36 -0500

    se añade activo de pagina

--- src/Components/ItemsS/ItemsS.jsx
+++ src/Components/ItemsS/ItemsS.jsx
@@ -1,10 +1,16 @@
-
-export const ItemsS = ({image,text ,onClick, isActive}) => {
-
+export const ItemsS = ({image, text, onClick, isActive}) => {
   return (
-    <button onClick={onClick} className={`mt-7 flex hover:bg-slate-600 hover:animate-clearIn w-[16vw] p-4 items-center rounded-bl-lg rounded-tr-lg ${isActive ? 'hover:animate-none bg-[#3c82f7] hover:bg-[#3c82f7]  ' : '' }`}
+    <button
+      onClick={onClick}
+      className={`mt-7 flex w-[16vw] p-4 items-center rounded-bl-lg rounded-tr-lg
+        ${isActive
+          ? 'bg-[#3c82f7] hover:bg-[#3c82f7]' 
+          : 'hover:bg-slate-600 hover:animate-clearIn'
+        }`
+      }
     >
-        <img className="w-5 " src={image} alt="" /><h1 className="ml-3 text-white ">{text}</h1>
+      <img className="w-5" src={image} alt="" />
+      <h1 className="ml-3 text-white">{text}</h1>
     </button>
-  )
+  );
 }
